6539537525d88c66b56fd0c28ea71dc9f5268978
Ángelcoy <angelalbertogarcia1@gmail.com>
2026-08-06 22:34:42 +0000

    add machine choice function and buttons js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("hello world")
+/*Declarar opciones en botones
+Declarar líneas de texto:
+
+-ronda
+
+-ganador
+
+-eleccion de jugador y máquina
+
+Variable para guardar la elección de usuario
+
+Contador de rondas
+
+Función para elección aleatoria de la máquina
+
+Escuchar de eventos al hacer click en una opción
+
+Comparador de elección entre usuario y máquina dando empate o ganador
+
+Función para sumar 1 al contador de rondas
+
+Función para modificar el las líneas de texto en base al ganador o empate
+
+Función para escuchar cuántas rondas van y si llega a 5 comparar victorias y declarar ganador
+
+Función para bloquear botones cuando se culminenas 5 rondas */
+
+const rock = document.querySelector("#btn-rock");
+const paper = document.querySelector("btn-paper");
+const scissors = document.querySelector("scissors");
+
+function getMachineChoice (){
+      const eleccion = Math.floor(Math.random() * 3) +1;
+     if (eleccion === 1 ) {
+        return "rock";
+     }
+     else if (eleccion ===2 ) {
+        return "paper";
+     }
+     else return "scissors";
+}
+
